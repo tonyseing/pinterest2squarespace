68026b8c66e012cbc99711204bc1bba48a8a8137
tony <tony@tony-Aspire-M3450.(none)>
2013-08-16 05:22:03 -0400

    will finish tomorrow

--- pinterest_to_squarespace.rb
+++ pinterest_to_squarespace.rb
@@ -1,5 +1,23 @@
 # this daemon checks for new pins and then posts them to a squarespace
 # blog
 
+require 'yaml'
+require 'pry'
+require "selenium-webdriver"
 
-pin_boards = ["http://pinterest.com/mattinnewyork/products-i-love.rss", "http://pinterest.com/mattinnewyork/interior-design/"]
+data = YAML.load_file('data.yml')
+pinterest_boards = data["pinterest_boards"]
+squarespace = data["squarespace_login"]
+
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "http://www.squarespace.com"
+
+element = driver.find_element(:class, 'customer-login')
+element.click
+element.send_keys squarespace.user
+element = driver.find_element(:id, 'yui_3_10_1_1_1376644582512_573')
+element.click
+element = driver.find_element(:id, 'field-input')
+
+
+
