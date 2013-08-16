4796efad59b23f227a67b7989c5408186c8724c5
tony <tony@tony-Aspire-M3450.(none)>
2013-08-16 16:39:03 -0400

    curse thing damn thing

--- pinterest_to_squarespace.rb
+++ pinterest_to_squarespace.rb
@@ -12,12 +12,25 @@ squarespace = data["squarespace_login"]
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "http://www.squarespace.com"
 
+# click login link
 element = driver.find_element(:class, 'customer-login')
 element.click
-element.send_keys squarespace.user
-element = driver.find_element(:id, 'yui_3_10_1_1_1376644582512_573')
-element.click
-element = driver.find_element(:id, 'field-input')
 
+binding.pry
+# find username field
+element = driver.find_element(:id, 'yui_3_10_1_1_1376682694457_1250')
+binding.pry
+# enter username
+element.send_keys squarespace["user"]
+
+# select password field
+element = driver.find_element(:id, 'yui_3_10_1_1_1376682320435_632')
+
+# enter password
+element.send_keys squarespace["password"]
+
+# click login button
+element = driver.find_element(:class, 'saveAndClose')
+element.click
 
 
